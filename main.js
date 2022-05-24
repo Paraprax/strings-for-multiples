8a13c6366b94310d026b14a776f4428592d18169
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-05-23 21:11:35 -0400

    Write initial string generator logic

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const devhaus105String = () => {};
+const devhaus105String = () => {
+  let output = "";
+
+  //generate a string of #'s 1 - 105:
+  for (var i = 1; i <= 105; i++) {
+    output = output.concat(`${i},`);
+  }
+
+  return output;
+};
+
+console.log(devhaus105String());
