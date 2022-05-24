f1b86f20c4ea22a95b54461635a6f87b03f3c96c
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-05-23 21:15:46 -0400

    Write conditional to add 'Devhaus' to string in place of all multiples of 3

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const devhaus105String = () => {
 
   //generate a string of #'s 1 - 105:
   for (var i = 1; i <= 105; i++) {
-    output = output.concat(`${i},`);
+    if (i % 3 == 0) {
+      output = output.concat(`Devhaus,`);
+    } else {
+      output = output.concat(`${i},`);
+    }
   }
 
   //trim off the last comma:
