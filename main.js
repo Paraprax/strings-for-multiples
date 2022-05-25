409635d35a549887c70d3aa7596b2f49cc2df9d6
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-05-24 20:47:51 -0400

    DRYing refactor w/ nested concatter function

--- main.js
+++ main.js
@@ -2,29 +2,40 @@
 const devhaus105String = () => {
   let output = "";
 
+  const concatter = (string) => {
+    output = output.concat(string);
+  };
+
   //generate a string of #'s 1 - 105:
   for (var i = 1; i <= 105; i++) {
     //swap key numbers with words:
     if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
-      output = output.concat(`iBlüm,`);
+      //3,5,7
+      concatter(`iBlüm,`);
     } else if (i % 5 == 0 && i % 7 == 0) {
-      output = output.concat(`Learning Model,`);
+      //5,7
+      concatter(`Learning Model,`);
     } else if (i % 3 == 0 && i % 7 == 0) {
-      output = output.concat(`Devhaus Model,`);
+      //3,7
+      concatter(`Devhaus Model,`);
     } else if (i % 3 == 0 && i % 5 == 0) {
-      output = output.concat(`Devhaus Learning,`);
+      //3,5
+      concatter(`Devhaus Learning,`);
     } else if (i % 7 == 0) {
-      output = output.concat(`Model,`);
+      //7
+      concatter(`Model,`);
     } else if (i % 5 == 0) {
-      output = output.concat(`Learning,`);
+      //5
+      concatter(`Learning,`);
     } else if (i % 3 == 0) {
-      output = output.concat(`Devhaus,`);
+      //3
+      concatter(`Devhaus,`);
     } else {
-      output = output.concat(`${i},`);
+      concatter(`${i},`);
     }
   }
 
-  //trim off the last comma:
+  //trim off the trailing comma:
   output = output.slice(0, -1);
   return output;
 };
